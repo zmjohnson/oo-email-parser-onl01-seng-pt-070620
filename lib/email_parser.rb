0c6e88590ce62c5d0a62aea1d2ee5314410f591e
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-29 19:29:26 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,22 +1,12 @@
-class EmailParser 
-  attr_accessor :name, :csv_emails
-  
-  def initialize(csv_emails)
-     @csv_emails = csv_emails
+class EmailParser
+  attr_accessor :emails
+
+  def initialize(emails)
+    self.emails = emails
   end
-  
-    def parse 
-    csv_emails.split.collect do |address|
-      address.split(',') 
-    end
-    .flatten.uniq 
-  end 
-end 
 
-emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
-parser1.parse 
-
-emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
-parser2.parse
+  def parse
+    results = self.emails.scan(/(\w\w*@\w\w*\.\w\w*)/)
+    results.flatten.uniq
+  end
+end
